8a149e11ed8a7729aa3b6bd07903093fcedc6758
mariacamilato <mariactobon@udea.edu.co>
2024-03-03 21:55:00 -0500

    cambio final

--- EJERCICIO.py
+++ EJERCICIO.py
@@ -1,10 +1,11 @@
 class Paciente:
+#constructor init que nos permite inicializar las variables(atributos) de la clase.
     def __init__(self):
         self.__nombre=""
         self.__cedula=int
         self.__genero=""
         self.__servicio=""
-
+#Los atributos de la clase Pasiente presentan nivel de encapsulamiento privado.
     def getNombre(self):
         return self.__nombre
     
@@ -16,7 +17,7 @@ class Paciente:
     
     def getServicio(self):
         return self.__servicio
-    
+# Los get nos permiten retornar los valores de dichos atributos privados.   
     def setNombre(self,nombre):
         self.__nombre=nombre 
     
@@ -28,11 +29,12 @@ class Paciente:
     
     def setServicio(self,servicio):
         self.__servicio=servicio 
-    
+#Por otro lado, los set nos permiten asignar valores a estos atribitos.
+#Los set y los get son los métodos que nos permiten utilizar los atributos privados fuera de la clase en los que se definen. 
 class Sistema:
     def __init__(self):
         self.__listadoPacientes=[]
-        
+#Constructor de la clase sistema con el contenedor de nuestros objetos pacientes, la lista.       
     def ingresarPacientes(self,paciente):
         self.__listadoPacientes.append(paciente)
         return True
@@ -48,24 +50,26 @@ class Sistema:
                 return None 
             for p in self.__listadoPacientes:
                 if cedula == p.getCedula():
-                    return p           
+                    return p  
 
     def verNumeroPacientes(self):
         return (f"Hasta el momento hay {len(self.__listadoPacientes)} pacientes registrados") 
     
     def salir(self):
         return (">>> Usted ha salido exitosamente del sistema <<<")
-    
+#Las anteriores funciones son los métodos de control de nuestro sistema.   
 def main(): 
+#La funcion main es el metodo principal, que nos permite ejecutar el código.
     mi_sistema=Sistema()
+#creamos el objeto sistema, para dar funcionalidad al menú.
     while True:
-        menu=input("Marque:\n1.Agregar paciente \n2.Ver datos de paciente \n3.Ver número de pacientes \n4.Salir ")
+        menu=input("Marque:\n1.Agregar paciente \n2.Ver datos de paciente \n3.Ver número de pacientes \n4.Salir >>> ")
         if menu=="1":
             cedula=input("Ingrese la cédula del paciente >>> ")
             if mi_sistema.verificarPacientes(cedula)== False:
                 nombre=input("Ingrese el nombre del paciente >>> ")
-                genero=input("Ingrese el género del paciente >>>")
-                servicio=input("Ingrese el servicio al cual ingresó el paciente >>>")
+                genero=input("Ingrese el género del paciente >>> ")
+                servicio=input("Ingrese el servicio al cual ingresó el paciente >>> ")
                 p=Paciente()
                 p.setCedula(cedula)
                 p.setNombre(nombre)
@@ -76,15 +80,33 @@ def main():
                  print("¡¡¡YA EXISTE UN PACIENTE CON ESE NÚMERO DE DOCUMENTO!!!")
 
         elif menu=="2":
-           cedula=input("Ingrese la cédula del paciente >>> ")
-           p= mi_sistema.verDatosPacientes(cedula)
-           if p != None:
-               print(f"La cédula del paciente es >>> {p.getCedula()}")
-               print(f"El nombre del paciente es >>>{p.getNombre()}")
-               print(f"El género del paciente es >>> {p.getGenero()}")
-               print(f"El servicio al cual ingresó el paciente es >>> {p.getServicio()}")
-           else:
-               print("¡¡¡NO EXISTE UN PACIENTE CON ESE NÚMERO DE DOCUMENTO!!!")
+           while True:
+               buscar=input("""Marque:
+                            1. BUSCAR PACIENTE ATRAVÉS DE SU CEDULA
+                            2. BUSCAR PACIENTE ATRAVÉS DE SU NOMBRE >>> """)
+               if buscar=="1":
+                    cedula=input("Ingrese la cédula del paciente >>> ")
+                    p= mi_sistema.verDatosPacientes(cedula)
+                    if p != None:
+                        print(f"La cédula del paciente es >>> {p.getCedula()}")
+                        print(f"El nombre del paciente es >>>{p.getNombre()}")
+                        print(f"El género del paciente es >>> {p.getGenero()}")
+                        print(f"El servicio al cual ingresó el paciente es >>> {p.getServicio()}")
+                        break
+                    else:
+                        print("¡¡¡NO EXISTE UN PACIENTE CON ESE NÚMERO DE DOCUMENTO!!!")
+                        continue
+               elif buscar=="2":
+                   nom=input("Ingrese en nombre del paciente completo o como lo recuerde >>> ")
+                   if (nom in p.getNombre())== True:
+                        print(f"La cédula del paciente es >>> {p.getCedula()}")
+                        print(f"El nombre del paciente es >>>{p.getNombre()}")
+                        print(f"El género del paciente es >>> {p.getGenero()}")
+                        print(f"El servicio al cual ingresó el paciente es >>> {p.getServicio()}")
+                        break
+                   else:
+                       print("¡¡¡NO EXISTE UN PACIENTE CON ESE NOMBRE!!!")
+                       continue
 
         elif menu=="3":
             print(mi_sistema.verNumeroPacientes())
@@ -94,7 +116,8 @@ def main():
             break
         else:
             print("INGRESÓ UNA OPCIÓN INCORRETA, INTENTE NUEVAMENTE")
-
+#Lo anterior es el menú de nuestro sistema.
+#En esta ocasión no se encontro ni herencia, ni polimorfismo.
 if __name__ == "__main__": 
     main()
 
--- VETERINARIA.py
+++ VETERINARIA.py
@@ -1,4 +1,7 @@
+from datetime import datetime 
+#Hacemos llamado a la libreria de datetime para el concepto de la fecha.
 class Mascota:
+#Este es el constructor de la clase mascota, que tiene la funcion de dar inicio a las variables o atributos.
     def __init__ (self):
         self.__nombre=""
         self.__historia=0 
@@ -6,7 +9,7 @@ class Mascota:
         self.__peso=""
         self.__fechaIngreso=""
         self.__listaMedicamentos=[]
-
+#Los atributos de esta clase tienen un nivel de encapsulamiento privado.
     def getNombre(self):
         return self.__nombre
     
@@ -24,6 +27,7 @@ class Mascota:
     
     def getListaMedicamentos(self):
         return self.__listaMedicamentos 
+#En esta sección se ubicaron los get, que retornaran los valores de los atributos de la clase Mascota.
     
     def setNombre(self,nombre):
         self.__nombre=nombre 
@@ -42,12 +46,26 @@ class Mascota:
 
     def setListaMedicamentos(self,lista):
         self.__listaMedicamentos=lista
-
-class Medicamento: 
+# por otra parte los set, de la clase Mascota nos permiten asignar los valores de los atributos y manejarlos fuera de la clase. 
+    def verificaMedicamento(self,medi):
+        for i in self.__listaMedicamentos:
+            if medi == i.getNombre():
+                return True
+        return False 
+    
+    def eliminarMedicamento(self,historia):
+        for i in self.__listaMedicamentos:
+            if historia == i.getNombre():
+                self.__listaMedicamentos.remove(i)
+                return True
+        return False 
+#Los metodos de la clase dados por las anteriores dos funciones.
+class Medicamento:
+#Esta seccion es de la clase Medicamento, y el constructor.
     def __init__(self):
         self.__nombre=""
         self.__dosis=""
-    
+#atributos con nivel de encapsulamiento privado.   
     def getNombre(self):
         return self.__nombre
     
@@ -59,52 +77,65 @@ class Medicamento:
     
     def setDosis(self,dosis):
         self.__dosis=dosis 
-
+#los get y set de la clase medicamento que nos permiten hacer uso de los atributos fuera de ella.
 class Sistema:
+#La clase sistema con sus elementos contenedores que serviran para ingresar nuestros objetos mascota.
     def __init__(self):
         self.__caninos=[]
         self.__felinos=[]
         self.__listadoMascotas={"caninos":self.__caninos, "felinos": self.__felinos}
-    
+#constructor init con sus atributos y nivel de encapsulamiento privado.  
     def ingresarCaninos(self,masc):
         self.__caninos.append(masc)
     
     def ingresarFelinos(self,masc):
         self.__felinos.append(masc)
 
-    def verFechaIngreso(self,masc,historia):
+    def verFechaIngreso(self,historia):
         for key in self.__listadoMascotas:
             for valor in self.__listadoMascotas[key]:
-                    if historia == masc.getHistoria():
-                        return masc.getFechaIngreso()
-        return None
+                    if historia == valor.getHistoria():
+                        return valor.getFechaIngreso()
+        return None 
 
     def verNumeroMascotas(self):
         return len(self.__caninos) + len(self.__felinos)
     
-    def verMedicamento(self,masc,historia):
-        for i in self.__listadoMascotas:
-            if historia == masc.getHistoria():
-                return masc.getListaMedicamentos()
-        return None 
-    
-    def eliminarMascota(self,masc,historia):
-        for i in self.__listadoMascotas:
-            if historia == masc.getHistoria():
-                self.__listadoMascotas.remove(masc)
-                return True 
-        return False 
-    
-    def verificarExiste(self,masc,historia):
+    def verMedicamento(self,historia):
         for key in self.__listadoMascotas:
             for valor in self.__listadoMascotas[key]:
-                if historia == masc.getHistoria():
+                if historia == valor.getHistoria():
+                    return valor.getListaMedicamentos()
+        return None 
+    
+    def eliminarMascota(self,historia):
+        for key in self.__listadoMascotas:
+            for valor in self.__listadoMascotas[key]:
+                if historia == valor.getHistoria():
+                    self.__listadoMascotas[key].remove(valor)
                     return True 
+        return False 
+    
+    def verificarExiste(self,historia):
+        for key in self.__listadoMascotas:
+            for valor in self.__listadoMascotas[key]:
+                    if historia == valor.getHistoria():
+                        return True 
         return False 
     
     def salir(self):
         return ("¡¡¡ Usted ha salido exitosamente de esta sección !!!")
-
+    
+    def fecha(self):
+        while True:
+            fecha=input("Ingrese la fecha en formato DD/MM/AA >>>")
+            try:
+                fe=datetime.strptime(fecha, "%d/%m/%y")
+                return fe
+                break 
+            except ValueError:
+                print("¡¡¡Formato incorrecto, intente nuevamente!!!")
+#Hasta esta parte se evidencian los métodos de nuestra clase sistema, que seran de utilidad en el menú del programa.
 def verifica(a):
     while True:
         try:
@@ -113,10 +144,12 @@ def verifica(a):
             break
         except ValueError:
             a=input("POR FAVOR INGRESE VALORES NÚMERICOS >>> ")
-
+#Esta funcion esta por fuera de todas las clases, se implementó para hacer verificación de 
+#valores númericos (solo no verifiqué el # de medicamentos a ingresar :( ...)
 def main():
+#Con Main, la funcion principal, hacemos la ejecución de nuestro programa.
     servicio=Sistema() 
-    masc=Mascota()
+    masc=Mascota() #Asignamos objetos, para incluir en el menú.
     while True:
         menu=input('''\nSeleccione: 
                    1. Agregar mascota
@@ -124,35 +157,47 @@ def main():
                    3. Ver número de mascotas en el servicio
                    4. Ver medicamentos administrados a la mascota
                    5. Eliminar Mascota
-                   6. Salir\n >>>''')
+                   6. Eliminar Medicamento de una mascota
+                   7. Salir\n >>>''')
         if menu =="1":
             if servicio.verNumeroMascotas() >= 10:
                 print("¡¡¡ No hay espacio para registro de mascotas !!!")
                 continue
             historia=(input("Ingrese el número de historia de la mascota >>> "))
             historia=verifica(historia)
-            if servicio.verificarExiste(masc,historia) == False:
-            
+            if servicio.verificarExiste(historia) == False:
                 nombre=input("Ingrese el nombre de la mascota >>> ")
                 tipo=input("""¿Qué tipo de mascota es? Marque
                            1.PARA CANINOS
                            2.PARA FELINOS >>> """)
                 peso=input("Ingrese el peso de la mascota en lb >>> ")
                 peso=verifica(peso)
-                fecha=input("Ingrese la fecha de ingreso de la mascota >>> ")
+                fecha=servicio.fecha()
                 cantidadm=int(input("Ingrese la cantidad de medicamentos que tiene la mascota >>> "))
                 listaMed=[]
 
                 for i in range (0,cantidadm):
-                    nombre_med=input(f"Ingrese el nombre del medicamento {i+1} >>> ")
-                    dosis=input(f"Ingrese la dosis del medicamento {i+1} en mm>>> ")
-                    dosis=verifica(dosis)
-                    medica=Medicamento()
-                    medica.setNombre(nombre_med)
-                    medica.setDosis(dosis)
-                    listaMed.append(medica)
+                    while True:
+                        nombre_med=input(f"Ingrese el nombre del medicamento {i+1} >>> ")
+                        medicamento_existente = False 
+                        for n in listaMed:
+                            if nombre_med == n.getNombre():
+                                #En esta parte se hace evidente el POLIMORFISMO
+                                #pues hay dos metodos con el mismo nombre
+                                #en clase Medicamento y en clase Mascota
+                                print("Este medicamento ya se encuentra en la lista")
+                                medicamento_existente = True  
+                                break
+                        if not medicamento_existente:
+                            dosis=input(f"Ingrese la dosis del medicamento {i+1} en mm >>> ")
+                            dosis=verifica(dosis)
+                            medica=Medicamento()
+                            medica.setNombre(nombre_med)#POLIMORFISMO
+                            medica.setDosis(dosis)
+                            listaMed.append(medica)
+                            break 
                 masc=Mascota()
-                masc.setNombre(nombre)
+                masc.setNombre(nombre)#POLIMORFISMO
                 masc.setHistoria(historia)
                 masc.setPeso(peso)
                 masc.setTipo(tipo)
@@ -168,7 +213,7 @@ def main():
         elif menu =="2":
             historia=input("Ingrese el número de historia de la mascota >>> ")
             historia=verifica(historia)
-            fecha= servicio.verFechaIngreso(masc,historia) 
+            fecha= servicio.verFechaIngreso(historia) 
             if fecha!=None:
                 print(f"La fecha de ingreso de la mascota fue >>> {fecha}")
             else:
@@ -176,30 +221,47 @@ def main():
         
         elif menu =="3":
             numero=servicio.verNumeroMascotas()
-            print(f"El número de mascotas ingresados hasta el momento es {numero}")
+            print(f"El número de mascotas ingresados hasta el momento es >>> {numero}")
         
         elif menu =="4":
             historia=input("Ingrese el número de historia de la mascota >>> ")
             historia=verifica(historia)
-            medicamento= servicio.verMedicamento(masc,historia)
+            medicamento= servicio.verMedicamento(historia)
             if medicamento!=None:
                 print("Los medicamentos de esta mascota son: >>> ")
                 for m in medicamento:
-                    print(f" Nombre de medicamento ---> {m.getNombre()}")
-                    print(f" Dosis de medicamento ---> {m.getDosis()}")
+                    print(f"Nombre de medicamento ---> {m.getNombre()}")
+                    print(f"Dosis de medicamento ---> {m.getDosis()}")
             else:
-                print("¡¡¡ No hay ninguna mascota que coincida con el número de historia !!! ")
+                print("¡¡¡ No hay ninguna mascota que coincida con el número de historia ingresado !!! ")
         
         elif menu =="5":
             historia=input("Ingrese el número de historia de la mascota >>> ")
             historia=verifica(historia)
-            eliminar=servicio.eliminarMascota(masc,historia)
+            eliminar=servicio.eliminarMascota(historia)
             if eliminar == True:
                 print("¡¡¡Mascota eliminada con éxito!!!")
             else:
-                print(" ¡¡¡ No hay ninguna mascota que coincida con el número de historia !!! ")
+                print(" ¡¡¡ No hay ninguna mascota que coincida con el número de historia ingresado !!! ")
         
-        elif menu =="6":
+        elif menu=="6":
+            historia=input("Ingrese el número de historia de la mascota >>> ")
+            historia=verifica(historia)
+            medicamento= servicio.verMedicamento(historia)
+            if medicamento!=None:
+                print("Los medicamentos de esta mascota son: >>> ")
+                for m in medicamento:
+                    print(f"Nombre de medicamento ---> {m.getNombre()}")
+                    print(f"Dosis de medicamento ---> {m.getDosis()}")
+                    x=input("Ingrese el nombre del medicamento que desea eliminar >>>")
+                    eliminado=masc.eliminarMedicamento(x)
+                    if eliminado== True:
+                        print("<<< Medicamento eliminado con éxito >>>")
+            else:
+                print(" ¡¡¡ No hay ninguna mascota que coincida con el número de historia ingresado !!! ")
+
+        
+        elif menu =="7":
                 salir=input(" SI DESEA SALIR MARQUE 1, DE LO CONTRARIO MARQUE 2 >>> ")
                 if salir=="1":
                     print(servicio.salir())
@@ -208,7 +270,7 @@ def main():
                     continue
         else:
             print("<<< OPCIÓN INCORRECTA, INTENTELO DE NUEVO >>>")
-
+#Los anteriores fragmentos contienen el menú de nuestro código, en esta ocasión no se presenta herencia.
 if __name__=="__main__":
     main()                
 
